95eceecb0fd584266b8d4773a7a72137fd782c11
vinjn <vinjn.z@gmail.com>
2016-02-27 14:18:00 +0800

    Add CoreSLAM files

--- src/SmallSlamApp.cpp
+++ src/SmallSlamApp.cpp
@@ -2,20 +2,34 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 
+// SLAM
+#include "CoreSLAM/CoreSLAM.h"
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+struct CoreSLAM
+{
+    ts_map_t map;
+    ts_randomizer_t randomizer;
+};
+
 class SmallSlamApp : public App {
-  public:
-	void setup() override;
-	void mouseDown( MouseEvent event ) override;
-	void update() override;
-	void draw() override;
+public:
+    void setup() override;
+    void mouseDown( MouseEvent event ) override;
+    void update() override;
+    void draw() override;
+    
+private:
+    CoreSLAM mCoreSLAM;
+    
 };
 
 void SmallSlamApp::setup()
 {
+    ts_map_init(&mCoreSLAM.map);
 }
 
 void SmallSlamApp::mouseDown( MouseEvent event )
@@ -28,7 +42,7 @@ void SmallSlamApp::update()
 
 void SmallSlamApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) ); 
+    gl::clear( Color( 0, 0, 0 ) );
 }
 
 CINDER_APP( SmallSlamApp, RendererGl )
